66d6d1f74cc131f46714b02b125ea61db39855f2
5wapnilmore <mswapnil383@gmail.com>
2024-05-23 10:47:19 +0530

    dsf

--- script.js
+++ script.js
@@ -8,14 +8,15 @@ const tDate = new Date;
 
 
 
-function Date() {
+function Date()
+{
     console.log(`${day} ${month} ${year}`);
     if (
-        !((day <= 31 && day >= 1 && day != null)
+        !((day <= 31 || day >= 1 || day != null)
         &&
-        !(month <= 12 && month >= 1 && month != null)
+        !(month <= 12 || month >= 1 || month != null)
         &&
-        !(year <= tDate.getFullYear && year >= 0 && year != null)))
+        !(year <= tDate.getFullYear || year >= 0 || year != null)))
     {
         alert("Please enter valid date !");
     }
