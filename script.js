069b2b455aa1e9ec8ec819fcb227fa4e137bec10
5wapnilmore <mswapnil383@gmail.com>
2024-05-24 22:13:14 +0530

    perfected validation (condition)

--- script.js
+++ script.js
@@ -1,28 +1,19 @@
-
-const day = document.getElementById("day").value;
-const month = document.getElementById("month").value;
-const year = document.getElementById("year").value;
-
-const tDate = new Date;
-
-
-
-
-function Date()
+function more()
 {
-    console.log(`${day} ${month} ${year}`);
+    const day = document.getElementById("day").value;
+    const month = document.getElementById("month").value;
+    const year = document.getElementById("year").value;
+
+    const tDate = new Date;
+    console.log(`${day} ${month} ${year}`);     
+
     if (
-        !((day <= 31 || day >= 1 || day != null)
-        &&
-        !(month <= 12 || month >= 1 || month != null)
-        &&
-        !(year <= tDate.getFullYear || year >= 0 || year != null)))
+        !((day <= 31 && day >= 1 && day != null)
+            &&
+            (month <= 12 && month >= 1 && month != null)
+            &&
+            (year <= tDate.getFullYear && year >= 0 && year != null)))
     {
         alert("Please enter valid date !");
     }
-    else
-    {
-        
-    }
-
 }
